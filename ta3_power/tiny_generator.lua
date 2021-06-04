bae6148336cb1f8596600d1252dd2c1de0c82065
Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>
2021-06-04 19:47:57 +0200

    Allow picking Tiny Generator with fuel
    Tiny Generator can now be collected and deployed with non-empty fuel tank.
    It also remebers engine cycle state, so nothing is wasted when moving
    node from one place to another. Item description provides information
    about fuel level and remaining engine cycles.
    
    Signed-off-by: Michal Cieslakiewicz <michal.cieslakiewicz@wp.pl>

--- ta3_power/tiny_generator.lua
+++ ta3_power/tiny_generator.lua
@@ -203,11 +203,32 @@ minetest.register_node("techage:tiny_generator", {
 	on_rotate = screwdriver.disallow,
 	is_ground_content = false,
 
-	after_place_node = function(pos)
+	after_place_node = function(pos, placer, itemstack)
 		local nvm = techage.get_nvm(pos)
 		local number = techage.add_node(pos, "techage:tiny_generator")
 		nvm.running = false
 		nvm.burn_cycles = 0
+		if itemstack then
+			local stack_meta = itemstack:get_meta()
+			if stack_meta then
+				local liquid_name = stack_meta:get_string("liquid_name")
+				local liquid_amount = stack_meta:get_int("liquid_amount")
+				if liquid_name ~= "" and fuel.burntime(liquid.name) and
+				   liquid_amount >= 0 and liquid_amount <= fuel.CAPACITY then
+					nvm.liquid = nvm.liquid or {}
+					nvm.liquid.name = liquid_name
+					nvm.liquid.amount = liquid_amount
+				end
+				local burn_cycles = stack_meta:get_string("burn_cycles")
+				if burn_cycles ~= "" then
+					nvm.burn_cycles = tonumber(burn_cycles)
+				end
+				local burn_cycles_total = stack_meta:get_string("burn_cycles_total")
+				if burn_cycles_total ~= "" then
+					nvm.burn_cycles_total = tonumber(burn_cycles_total)
+				end
+			end
+		end
 		State:node_init(pos, nvm, number)
 		M(pos):set_string("formspec", formspec(State, pos, nvm))
 		M(pos):set_int("outdir", networks.side_to_outdir(pos, "R"))
@@ -221,11 +242,30 @@ minetest.register_node("techage:tiny_generator", {
 		techage.del_mem(pos)
 	end,
 
+	preserve_metadata = function(pos, oldnode, oldmetadata, drops)
+		local nvm = techage.get_nvm(pos)
+		if (nvm.liquid and nvm.liquid.name) or nvm.burn_cycles > 0 then
+			local meta = drops[1]:get_meta()
+			if nvm.liquid and nvm.liquid.name and nvm.liquid.amount > 0 then
+				-- generator tank is not empty
+				meta:set_string("liquid_name", nvm.liquid.name)
+				meta:set_int("liquid_amount", nvm.liquid.amount)
+			end
+			local fuel_text = "fuel: " .. tostring(nvm.liquid and nvm.liquid.amount or 0) .. "/" .. tostring(fuel.CAPACITY)
+			meta:set_string("burn_cycles", tostring(nvm.burn_cycles))
+			local cycles_text = "cycles: " .. tostring(nvm.burn_cycles)
+			if nvm.burn_cycles_total then
+				meta:set_string("burn_cycles_total", tostring(nvm.burn_cycles_total))
+				cycles_text = cycles_text .. "/" .. tostring(nvm.burn_cycles_total)
+			end
+			meta:set_string("description", S("TA3 Tiny Power Generator") .. " (" .. fuel_text .. ", " .. cycles_text .. ")")
+		end
+        end,
+
 	on_receive_fields = on_receive_fields,
 	on_rightclick = on_rightclick,
 	on_punch = fuel.on_punch,
 	on_timer = node_timer,
-	can_dig = fuel.can_dig,
 	liquid = liquid_def,
 	networks = net_def,
 })
