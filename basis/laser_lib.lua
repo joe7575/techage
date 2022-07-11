87dfe8e6512b40f68097c12088e4dbf88380b35e
Joachim Stolberg <joe.stolberg@gmx.de>
2022-07-11 20:20:00 +0200

    Fix use_texture_alpha issues

--- basis/laser_lib.lua
+++ basis/laser_lib.lua
@@ -98,7 +98,7 @@ for _, size in ipairs(SIZES) do
 				"techage_laser.png",
 				"techage_laser.png",
 			},
-			use_texture_alpha = techage.BLEND,
+			use_texture_alpha = true,
 			physical = false,
 			collide_with_objects = false,
 			pointable = false,
