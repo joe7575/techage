f0c0ee3d75f3dc91f90e6e86b0bd4d45af494ec8
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2023-04-11 10:44:21 +0200

    fix unknown item drops

--- items/moreblocks.lua
+++ items/moreblocks.lua
@@ -53,8 +53,8 @@ local NodeNames = {
 
 	"techage:basalt_glass",
 	"techage:basalt_glass2",
-	"techage:bauxite_stone",
 	"techage:bauxite_cobble",
+	"techage:bauxite_stone",
 
 	"techage:cement_block",
 }
@@ -64,6 +64,9 @@ if(minetest.get_modpath("moreblocks")) then
 		local ndef = minetest.registered_nodes[name]
 		if ndef then
 			ndef = table.copy(ndef)
+			if ndef.drop then -- this fixes https://github.com/fluxionary/minetest-moreblocks/issues/19
+				ndef.drop = nil
+			end
 			local subname = string.split(name, ":")[2]
 			ndef.sunlight_propagates = true
 			ndef.groups.not_in_creative_inventory = 1
