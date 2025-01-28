f5c848f1eca21231c70684ca8eb7e7170f7d5ee0
Joachim Stolberg <joe.stolberg@gmx.de>
2025-01-28 21:20:33 +0100

    Add CRT monitor

--- icta_controller/monitor.lua
+++ icta_controller/monitor.lua
@@ -13,41 +13,44 @@
 ]]--
 
 local S = techage.S
+local M = minetest.get_meta
 
-local NUM_ROWS = 14
-local NUM_CHARS = 28
-local COLOR = "#DFD700" --"#CCCCCC" -- "#33FF00" -- "#FFB000"
-local RADIUS = 6
+local MENU = {
+	{
+		type = "dropdown",
+		choices = "16x8,20x10,24x12,28x14,32x16,36x18,40x20",
+		name = "resolution",
+		label = S("Resolution"),
+		tooltip = S("Select resolution of the monitor in characters x lines"),
+		default = "8",
+		values = {8,10,12,14,16,18,20},
+	},
+	{
+		type = "dropdown",
+		choices = "green,amber,yellow,white",
+		name = "color",
+		label = S("Color"),
+		tooltip = S("Select color of the screen text"),
+		default = "#2DD204",
+		values = {"#2DD204","#F8C800","#DFD700","#CCCCCC"},
+	},
+}
 
 local function update(pos, objref)
 	pos = vector.round(pos)
 	local nvm = techage.get_nvm(pos)
 	local t = core.get_us_time()
-	local texture = lcdlib.make_mono_texture(nvm.text or {}, NUM_CHARS, NUM_ROWS, COLOR)
-	objref:set_properties({ textures = {texture}, visual_size = {x=0.90, y=0.90} })
+	lcdlib.on_mono_display_update(pos, objref, nvm.text or {})
 	t = core.get_us_time() - t
-	print("time =", t, "#texture =", #texture)
+	print("time =", t)
 end
 
 local function on_timer(pos)
 	local mem = techage.get_mem(pos)
 	mem.ticks = mem.ticks or 0
-
 	if mem.ticks > 0 then
-		local node = minetest.get_node(pos)
-		-- check if display is loaded and a player in front of the display
-		if node.name ~= "ignore" then
-			local dir = minetest.facedir_to_dir((node.param2 + 2) % 4)
-			dir.y = 0
-			local pos2 = vector.add(pos, vector.multiply(dir, RADIUS))
-			for _, obj in pairs(minetest.get_objects_inside_radius(pos2, RADIUS)) do
-				if obj:is_player() then
-					lcdlib.update_entities(pos)
-					mem.ticks = mem.ticks - 1
-					break
-				end
-			end
-		end
+		lcdlib.update_entities(pos)
+		mem.ticks = mem.ticks - 1
 	end
 	return true
 end
@@ -67,56 +70,148 @@ minetest.register_node("techage:ta3_monitor", {
 	node_box = {
 		type = "fixed",
 		fixed = {
-			-- {-12/32, -16/32, -16/32,  12/32, -14/32, 16/32},
-			-- {-12/32, -14/32,  -3/32,  12/32,   6/32, 16/32},
-			-- {-10/32, -12/32,  14/32,  10/32,   4/32, 18/32},
-			-- {-12/32,   4/32,  -4/32,  12/32,   6/32, 16/32},
-			-- {-12/32, -16/32,  -4/32, -10/32,   6/32, 16/32},
-			-- { 10/32, -16/32,  -4/32,  12/32,   6/32, 16/32},
-			-- {-12/32, -14/32,  -4/32,  12/32, -12/32, 16/32},
-			{-16/32, -16/32, -15/32,  16/32, 16/32, 15/32},
-		},
-	},
-	selection_box = {
-		type = "fixed",
-		fixed = {
-			{-16/32, -16/32, -15/32,  16/32, 16/32, 15/32},
+			{-16/32, -16/32, -16.05/32,  16/32, 16/32, 16/32},
 		},
 	},
 	paramtype = "light",
 	use_texture_alpha = techage.CLIP,
 	sunlight_propagates = true,
-	light_source = 6,
+	light_source = 8,
+	ta3_formspec = MENU,
+	ta_after_formspec = function(pos, fields, playername)
+		if fields.save then
+			lcdlib.update_entities(pos)
+			local resolution = M(pos):get_int("resolution")
+			local cycle_time = resolution * resolution / 64
+			print("cycle_time =", cycle_time)
+			minetest.get_node_timer(pos):start(cycle_time)
+		end
+	end,
 
 	display_entities = {
 		["techage:monitor_entity"] = { 
-			depth = -0.48,
+			depth = -0.51,
 			yoffs = 0.025,
-			chars = NUM_CHARS,
-			rows = NUM_ROWS,
-			color = COLOR,
+			size = {x=0.90, y=0.90},
 			on_display_update = update
 		},
 	},
 
 	after_place_node = function(pos, placer)
 		local number = techage.add_node(pos, "techage:ta3_monitor")
-		local meta = minetest.get_meta(pos)
+		local meta = M(pos)
 		meta:set_string("number", number)
+		meta:set_int("resolution", 8)
+		meta:set_string("color", "#33FF00")
 		meta:set_string("infotext", S("TA3 CRT Monitor no: ")..number)
 		local nvm = techage.get_nvm(pos)
+		nvm.num_rows = 14
 		nvm.text = {"My", "Techage","TA3", "CRT Monitor", "No: "..number}
+		lcdlib.update_entities(pos)
 		minetest.get_node_timer(pos):start(1)
 	end,
 
 	after_dig_node = function(pos, oldnode, oldmetadata)
 		techage.remove_node(pos, oldnode, oldmetadata)
+		techage.del_mem(pos)
 	end,
 
 	on_timer = on_timer,
-	on_construct = lcdlib.on_construct,
 	on_destruct = lcdlib.on_destruct,
 	on_rotate = lcdlib.on_rotate,
+	groups = {cracky=2, crumbly=2},
+	is_ground_content = false,
+	sounds = default.node_sound_glass_defaults(),
+})
+
+minetest.register_node("techage:ta3_monitorXL", {
+	description = S("TA3 CRT Monitor XL"),
+	tiles = {-- up, down, right, left, back, front
+		'techage_monitor_side.png',
+		'techage_monitor_side.png',
+		'techage_monitor_side.png',
+		'techage_monitor_side.png',
+		'techage_monitor_side.png',
+		'techage_monitor_front2.png',
+	},
+	drawtype = "nodebox",
+	paramtype2 = "facedir",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-16/32, -16/32, -16.05/32,  16/32, 16/32, 16/32},
+		},
+	},
+	paramtype = "light",
+	use_texture_alpha = techage.CLIP,
+	sunlight_propagates = true,
+	light_source = 8,
+	ta3_formspec = MENU,
+	ta_after_formspec = function(pos, fields, playername)
+		if fields.save then
+			lcdlib.update_entities(pos)
+			local resolution = M(pos):get_int("resolution")
+			local cycle_time = resolution * resolution / 64
+			print("cycle_time =", cycle_time)
+			minetest.get_node_timer(pos):start(cycle_time)
+		end
+	end,
+	display_entities = {
+		["techage:monitor_entity"] = { 
+			depth = -0.51,
+			size = {x=2.8, y=2.8},
+			on_display_update = update
+		},
+	},
+
+	after_place_node = function(pos, placer)
+		local number = techage.add_node(pos, "techage:ta3_monitorXL")
+		local meta = M(pos)
+		meta:set_string("number", number)
+		meta:set_int("resolution", 8)
+		meta:set_string("color", "#33FF00")
+		meta:set_string("infotext", S("TA3 CRT Monitor no: ")..number)
+		local nvm = techage.get_nvm(pos)
+		nvm.text = {"My", "Techage","TA3", "CRT Monitor", "No: "..number}
+		lcdlib.update_entities(pos)
+		minetest.get_node_timer(pos):start(1)
+	end,
+
+	after_dig_node = function(pos, oldnode, oldmetadata)
+		techage.remove_node(pos, oldnode, oldmetadata)
+		techage.del_mem(pos)
+	end,
+
+	on_timer = on_timer,
+	on_destruct = lcdlib.on_destruct,
+	on_rotate = lcdlib.on_rotate,
+	groups = {cracky=2, crumbly=2},
+	is_ground_content = false,
+	sounds = default.node_sound_glass_defaults(),
+})
+
+minetest.register_node("techage:ta3_monitorXL2", {
+	description = S("TA3 CRT Monitor Block"),
+	tiles = {-- up, down, right, left, back, front
+		'techage_monitor_side.png',
+		'techage_monitor_side.png',
+		'techage_monitor_side.png',
+		'techage_monitor_side.png',
+		'techage_monitor_side.png',
+		'techage_monitor_front2.png',
+	},
+	drawtype = "nodebox",
+	paramtype2 = "facedir",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-16/32, -16/32, -16.05/32,  16/32, 16/32, 16/32},
+		},
+	},
+	paramtype = "light",
+	use_texture_alpha = techage.CLIP,
+	sunlight_propagates = true,
+	light_source = 8,
 	groups = {cracky=2, crumbly=2},
 	is_ground_content = false,
 	sounds = default.node_sound_glass_defaults(),
@@ -134,6 +229,7 @@ minetest.register_craft({
 local function add_line(pos, payload, cycle_time)
 	local nvm = techage.get_nvm(pos)
 	local mem = techage.get_mem(pos)
+	local num_rows = M(pos):get_int("resolution") or 8
 	nvm.text = nvm.text or {}
 	mem.ticks = mem.ticks or 0
 	local str = tostring(payload) or "oops"
@@ -142,7 +238,7 @@ local function add_line(pos, payload, cycle_time)
 		mem.ticks = cycle_time
 	end
 
-	while #nvm.text >= NUM_ROWS do
+	while #nvm.text >= num_rows do
 		table.remove(nvm.text, 1)
 	end
 	table.insert(nvm.text, str)
@@ -151,6 +247,7 @@ end
 local function write_row(pos, payload, cycle_time, beduino)
 	local nvm = techage.get_nvm(pos)
 	local mem = techage.get_mem(pos)
+	local num_rows = M(pos):get_int("resolution") or 8
 	local str, row
 
 	nvm.text = nvm.text or {}
@@ -169,7 +266,7 @@ local function write_row(pos, payload, cycle_time, beduino)
 	end
 
 	if row < 1 then row = 1 end
-	if row > NUM_ROWS then row = NUM_ROWS end
+	if row > num_rows then row = num_rows end
 
 	while #nvm.text < row do
 		table.insert(nvm.text, "")
@@ -189,7 +286,7 @@ local function clear_screen(pos, cycle_time)
 	nvm.text = {}
 end
 
-techage.register_node({"techage:ta3_monitor"}, {
+techage.register_node({"techage:ta3_monitor", "techage:ta3_monitorXL"}, {
 	on_recv_message = function(pos, src, topic, payload)
 		if topic == "add" then  -- add one line and scroll if necessary
 			add_line(pos, payload, 1)
@@ -200,7 +297,6 @@ techage.register_node({"techage:ta3_monitor"}, {
 		end
 	end,
 	on_beduino_receive_cmnd = function(pos, src, topic, payload)
-		print("on_beduino_receive_cmnd", topic, payload)
 		if topic == 67 then  -- add one line and scroll if necessary
 			add_line(pos, payload, 1)
 		elseif topic == 68 then  -- overwrite the given row
@@ -211,6 +307,9 @@ techage.register_node({"techage:ta3_monitor"}, {
 			return 2
 		end
 		return 0
+	end,
+	on_node_load = function(pos)
+		lcdlib.update_entities(pos)
 	end,
 })
 
